13887a8cad96f78dc774d9602c039f62efb0873b
Nikita Dmitriev <nikdmit8@gmail.com>
2020-12-02 16:53:21 +0300

    Initial commit. Liskov Substitution principle case study.

---
+++ LiskovSubstitutionStudy/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace LiskovSubstitutionStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager accountingVP = new Manager();
+
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
+
+            Employee emp = new Employee();
+
+            emp.FirstName = "Michael";
+            emp.LastName = "Scott";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
